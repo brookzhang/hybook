6e5bac39fd3c92f1a88e22624222b879bbd817cb
brookzhang <brook.realize@gmail.com>
2011-07-12 14:06:33 +0800

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hybook::Application.config.secret_token = 'd41f4c4fd820a7787037440610ea661b5f5c838a22f7291c7bd60c9c9b67652c880e2cb67ec7f72b93355ab6e7a760e8c7d5a7c9059244d3ba3f5534671122e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hybook::Application.config.session_store :cookie_store, key: '_hybook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hybook::Application.config.session_store :active_record_store
