57fdfbdb5a9430aae88c0e317944382f2301dfed
brookzhang <brook.realize@gmail.com>
2011-07-27 17:52:42 +0800

    reuse the old style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:session][:email] , params[:session][:password])
     
     if user.nil?
-      flash.now[:error] = "Invalid email/password combination."
+      flash.now[:error] = "Invalid mail/password"
       @title = "Sign in"
       render 'new'
     else
